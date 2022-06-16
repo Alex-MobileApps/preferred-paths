d2acd2181c3e9e3bedb48bd600fe80a5fa34cc35
Alex Hocking <alexhockingapps@gmail.com>
2022-06-16 21:32:27 +1000

    Added parameter summary printout to ozstar script

--- ozstar_train.py
+++ ozstar_train.py
@@ -51,11 +51,15 @@ if __name__ == "__main__":
     fns = args['fns']
     num_fns = len(fns)
 
-    # Print summary of selected input arguments
-    print('\n====================')
+    # Print and save summary of selected input arguments
+    summary = '\n====================\n'
     subj_name = f'x{len(subj)}' if len(subj) > 1 else f's{str(subj[0] + 1).zfill(3)}'
-    print(f'Running with parameters:', f'device = {device}', f'res = {res}', f'subj = {subj_name}', f'epochs = {epoch}', f'batch_size = {batch}', f'samples = {sample}', f'hidden_units = {hidden_units}', f'lr = {lr}', f'save_path = {save_path}', f'load_path = {load_path}', f'log_output = {log}', f'path_method = {path_method}', f'rand_seed = {seed}', f'nn_init_weight = {nn_init_weight}', f'const_sig = {const_sig}', f'pos_only = {pos_only}', f'Functions = {num_fns} ({", ".join([f for f in fns])})', sep='\n')
-    print('====================', flush=True)
+    summary += '\n'.join([f'Running with parameters:', f'device = {device}', f'res = {res}', f'subj = {subj_name}', f'epochs = {epoch}', f'batch_size = {batch}', f'samples = {sample}', f'hidden_units = {hidden_units}', f'lr = {lr}', f'save_path = {save_path}', f'load_path = {load_path}', f'log_output = {log}', f'path_method = {path_method}', f'rand_seed = {seed}', f'nn_init_weight = {nn_init_weight}', f'const_sig = {const_sig}', f'pos_only = {pos_only}', f'Functions = {num_fns} ({", ".join([f for f in fns])})'])
+    summary += '\n===================='
+    if save_path:
+        with open(save_path + '.txt', 'w') as file:
+            file.write(summary)
+    print(summary, flush=True)
     if log:
         print('Reading files...')
 
